e1e6b1c5026d079d0d3b91637cf40e9cc6271c5f
faik.bagci@hotmail.de <faik.bagci@hotmail.de>
2024-05-10 11:20:11 +0200

    erledigt

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import readlineSync from "readline-sync";
+
+const name = readlineSync.question("Wie heißt du? ");
+const birthday = readlineSync.question("In welchem Jahr wurdest du geboren? ");
+const born = readlineSync.question("In welcher Stadt wurdest du geboren? ");
+const live = readlineSync.question("Wo lebst du jetzt? ");
+const year = readlineSync.question("Was ist das aktuelle Jahr? ");
+
+let age = year - birthday;
+
+console.log("\nHallo! Mein Name ist " + name + ". Ich wurde vor " + age + " Jahren in " + born + " geboren. Jetzt lebe ich in " + live + ".")
